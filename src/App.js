3141f64ad05ec52085338b4c9521e0d3c52086a1
Dustin Kovac <dustin.kovac@callawaycloudconsulting.com>
2021-11-20 15:01:15 -0700

    - yay

--- src/App.js
+++ src/App.js
@@ -7,15 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Winter is Coming to a Bar Near You!
+          <br></br>
+          Stay Tuned
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
         </a>
       </header>
     </div>
