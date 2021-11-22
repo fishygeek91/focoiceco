4621ea1e2cefafee0bdda5ec9a062aff4018c368
Dustin Kovac <dustin.kovac@callawaycloudconsulting.com>
2021-11-22 11:03:27 -0700

    - lol

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Winter is Coming to a Bar Near You!
+          Break the ice! At Bar Near You!
           <br></br>
           Stay Tuned
         </p>
